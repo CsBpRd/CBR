069f5d43aca99830754652341e4d0ca6383f97fd
CBR <csbprd@outlook.com>
2026-06-27 18:20:27 +0800

    修复：恢复原始 gjx.html/pu.html，导航直连静态页面，删除多余的 React 空壳路由

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import ThemeToggle from './components/ThemeToggle'
 import Home from './pages/Home'
-import Gjx from './pages/Gjx'
-import Pu from './pages/Pu'
-import Zgmz from './pages/Zgmz'
 import Tonglan from './pages/Tonglan'
-import Photopicker from './pages/Photopicker'
 
 export default function App() {
   return (
@@ -16,11 +12,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/gjx" element={<Gjx />} />
-        <Route path="/pu" element={<Pu />} />
-        <Route path="/zgmz" element={<Zgmz />} />
         <Route path="/tonglan" element={<Tonglan />} />
-        <Route path="/photopicker" element={<Photopicker />} />
       </Routes>
       <Footer />
     </>
